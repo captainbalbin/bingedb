01ee98f3f6af7b7218f9da4ff00c2882c6a0e53f
captainbalbin <albin@sjostrand.dev>
2025-05-30 16:37:34 +0200

    Update components

---
+++ src/components/Test.svelte
@@ -0,0 +1,15 @@
+<!-- @format -->
+<script>
+  let message = "Hello from Test.svelte!";
+</script>
+
+<main>
+  <h1>{message}</h1>
+</main>
+
+<style>
+  main {
+    padding: 1rem;
+    font-family: sans-serif;
+  }
+</style>
